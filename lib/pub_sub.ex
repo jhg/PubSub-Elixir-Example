0a347ab130cf3687ea0915c905aa4793ef6821e7
Jesus Hernandez <jesushernandez@protonmail.com>
2020-06-30 01:01:49 +0200

    Initial commit

---
+++ lib/pub_sub.ex
@@ -0,0 +1,18 @@
+defmodule PubSub do
+  @moduledoc """
+  Documentation for PubSub.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PubSub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
